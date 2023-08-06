4d7d38d4abb05b3242b35d6e140543dc2001f2a9
Saeid <dev@basirnia.xyz>
2023-08-06 23:03:36 +0200

    add CircularLoadingView

---
+++ Sources/LoadingButton/CircularLoadingView.swift
@@ -0,0 +1,58 @@
+//
+//  CircularLoadingView.swift
+//  
+//
+//  Created by Saeid Basirnia on 2023-08-05.
+//
+
+import SwiftUI
+
+struct CircularLoadingView: View {
+    private var style: Style
+    @State private var isLoading = false
+
+    init(style: Style) {
+        self.style = style
+    }
+
+    var body: some View {
+        Circle()
+            .trim(from: 0, to: 0.6)
+            .stroke(
+                style.backgroundColor,
+                style: StrokeStyle(
+                    lineWidth: style.strokeWidth,
+                    lineCap: .round,
+                    lineJoin: .round
+                )
+            )
+            .frame(
+                width: style.height - 20,
+                height: style.height - 20
+            )
+            .rotationEffect(
+                Angle(degrees: isLoading ? 360 : 0)
+            )
+            .animation(
+                Animation
+                    .linear.speed(0.4)
+                    .repeatForever(autoreverses: false),
+                value: isLoading
+            )
+            .opacity(isLoading ? 1 : 0)
+            .onAppear {
+                isLoading = true
+            }
+    }
+}
+
+struct CircularLoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            CircularLoadingView(
+                style: .default
+            )
+            .frame(width: 50, height: 50)
+        }
+    }
+}
